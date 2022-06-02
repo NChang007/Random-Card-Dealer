599f1f79311e78d2d0b9545d963b79e204f80d66
NChang007 <nelsonc924@gmail.com>
2022-06-02 00:24:39 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector('.card').innerHTML = genRanNum();
+  document.querySelector('.card').classList.add([genRanSuit()]);
 };
+
+
+let genRanNum = () => {
+  let number = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  let ranNum = Math.floor(Math.random() * number.length);
+  return number[ranNum];
+};
+
+let genRanSuit = () => {
+  let suit = ['diams', 'spade', 'hearts', 'clubs'];
+  let ranSuit = Math.floor(Math.random() * suit.length);
+  return suit[ranSuit];
+};
+
+// let btn = document.querySelector('.myBtn').addEventListener('click'){
+//   if()
+// };
+
+// function hasClass(){
+//   let card = document.querySelector('.card');
+//   if($(card).hasClass('spade')){
+//     $(card).removeClass('spade');
+//     document.querySelector('.card').classList.add([genRanSuit()]);
+//   } else if($(card).hasClass('clubs')){
+//     $(card).removeClass('clubs');
+//     document.querySelector('.card').classList.add([genRanSuit()]);
+//   } else if($(card).hasClass('heart')){
+//     $(card).removeClass('heart');
+//     document.querySelector('.card').classList.add([genRanSuit()]);
+//   } else if($(card).hasClass('diams')){
+//     $(card).removeClass('diams');
+//     document.querySelector('.card').classList.add([genRanSuit()]);
+//   } 
+// }
